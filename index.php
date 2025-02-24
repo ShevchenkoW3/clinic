0647fb63f74d81da4a2508a130154ba47e66cb7a
ShewchenkoW3 <shevdan03@yandex.ru>
2025-02-24 11:46:30 +0300

    Регистратор

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php include 'temp/head.php';
 include 'temp/database.php';
 session_start();
-if(!empty($_SESSION)){
+if(!empty($_SESSION['id_role'])){
 $id_role = $_SESSION['id_role'];
 if($id_role == 2){
     include 'temp/nav_registrator.php';
 }
 elseif($id_role == 5){
-    //Вывод навигации специалиста обработки данных
+    include 'temp/nav_specialist.php';
 }
-else(){
+else{
     //Вывод навигации пациента
 }
 }
@@ -17,16 +17,102 @@ else{
     include 'temp/nav.php';
 }
 ?>
+    <main>
 <div class="container">
-    <div class="row">
-        <div class="col-2"></div>
-        <div class="col-8">
-            <div class="text-center">
-                <h1></h1>
+    
+
+<section id="banner">
+    <div id="carouselExampleDark" class="carousel carousel-white slide">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleWhite" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleWhite" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleWhite" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="img/123.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption  d-md-block">
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="img/баннер2.png" class="d-block w-100" alt="...">
+                <div class="carousel-caption  d-md-block">
+                    <h1 style="color:rgb(2,36,63)">ГИБКАЯ СИСТЕМА СКИДОК!</h1>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="img/баннер3.png" class="d-block w-100" alt="...">
+                <div class="carousel-caption  d-md-block">
+                    <h1>ИНДИВИДУАЛЬНЫЙ ПОДХОД ДЛЯ КАЖДОГО ПАЦИЕНТА!</h1>
+                </div>
             </div>
         </div>
-        <div class="col-2"></div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Предыдущий</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Следующий</span>
+        </button>
+    </div>
+    <div class="row"><br><br>
+</section>
+<h2 class="name" style="text-align: center">УСЛУГИ ЧАСТНОЙ ПОЛИКЛИНИКИ</h2>
+<section id="price">
+    <div class="row">
+        <div class="col-lg-1"></div>
+        <div class="col-lg-10">
+            <div class="row row-cols-1  row-cols-md-6 g-4">
+                <?php
+                $sql = "SELECT * FROM usluga";
+                $result = $mysqli->query($sql);
+                foreach ($result as $row)  {
+                    echo '<div class="col">
+      <div class="card h-100">
+      <div class="card">
+         <img src="img/'.$row['img'].'" class="card-img-top" alt="...">
+         </div>
+      <div class="card-body">
+         <p class="card-title"style="color:rgb(2,36,63)"><b>'.$row['name_usl'].'</b></p>
+         <p class="card-text"style="color:rgb(2,36,63)">'.$row['price'].'Руб.</p> 
+      </div>
+    </div>
+  </div>';
+                }
+                ?>
+            </div>
+        </div>
+        <div class="col-lg-1"></div>
+    </div>
+</section><br><br>
+<h2 class="name" style="text-align: center">О КЛИНИКЕ</h2>
+<section id="price">
+    <div class="row">
+        <div class="col-lg-2"></div>
+        <div class="col-lg-8">
+            <div class="row row-cols-1  row-cols-md-3 g-3">
+                <img src="img/2.png" class="img-same-size" alt="...">
+                <img src="img/3.png" class="img-same-size" alt="...">
+                <img src="img/4.png" class="img-same-size" alt="...">
+            </div>
+        </div>
+        <div class="col-lg-2"></div>
+    </div><br>
+    <div class="row">
+        <div class="col-lg-2"></div>
+        <div class="col-lg-8">
+            <div class="row row-cols-1  row-cols-md-3 g-3">
+                <img src="img/5.png" class="img-same-size" alt="...">
+                <img src="img/6.png" class="img-same-size" alt="...">
+                <img src="img/7.jpg" class="img-same-size" alt="...">
+            </div>
+        </div>
+        <div class="col-lg-2"></div>
+    </div>
+</section>
+</main>
 
     </div>
-</div>
+</div><br><br>
 <?php include 'temp/footer.php';?>
---
+++ uslugi.php
@@ -0,0 +1,50 @@
+<?php
+include 'temp/head.php';
+include 'temp/nav_registrator.php';
+include 'temp/database.php';
+$sql = "select * from usluga";
+$result = $mysqli->query($sql);
+?>
+<div class="container" class="margin-top: 20px">
+    <div class="row">
+        <div class="col-lg-12">
+            <h2 class="text-center">Услуги</h2>
+            <a href="new_usluga.php" role="button" class="btn btn-primary">Добавить услугу</a>
+        </div>
+    </div><br>
+    <div class="row">
+        <div class="col-lg-12">
+        <table class="table">
+                <thead>
+                    <tr class="table-primary">
+                        <th>№</th>
+                        <th></th>
+                        <th>Название услуги</th>
+                        <th>Стоимость</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody class="table-group-divider">
+                    <?php
+                    if(!empty($result)){
+                        foreach($result as $row){
+                            $id_usluga = $row['id_usluga'];
+                            $img = $row['img'];
+                            $name_usl = $row['name_usl'];
+                            $price = $row['price'];
+                            echo '
+                            <tr>
+                            <td>'.$id_usluga.'</td>
+                            <td><img class="img" src="img/'.$img.'" alt="" width="102" height="99"></td>
+                            <td>'.$name_usl.'</td>
+                            <td>'.$price.'</td>;
+                            <td><a href="edit.php?id_usluga='.$id_usluga.'">Изменить цену</a></td>
+                            </tr>';
+                            }
+                        }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
