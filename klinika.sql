8419c36f93400aebc67225f1488b2bd002c46863
ShewchenkoW3 <shevdan03@yandex.ru>
2025-02-24 13:28:23 +0300

    Update klinika.sql

--- klinika.sql
+++ klinika.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Фев 22 2025 г., 19:34
+-- Время создания: Фев 24 2025 г., 13:28
 -- Версия сервера: 5.7.39
 -- Версия PHP: 8.0.22
 
@@ -84,9 +84,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id_user`, `fio`, `email`, `login`, `password`, `id_role`) VALUES
-(1, 'Шевченко Данила Александрович', 'registrator@yandex.ru', 'registrator', 'registrator', 2),
-(2, 'Краснов Андрей Глебович', 'specialist@yandex.ru', 'specialist', 'specialist', 5),
-(3, 'Иванов Иван Иванович', 'pacient1@yandex.ru', 'pacient1', 'pacient1', 4);
+(1, 'Краснов Андрей Глебович', 'andry.k2005@mail.ru', 'andrew', '123123', 5),
+(2, 'Шевченко Данила Александрович', 'registrator@yandex.ru', 'registrator', 'registrator', 2);
 
 -- --------------------------------------------------------
 
@@ -98,7 +97,7 @@ CREATE TABLE `usluga` (
   `id_usluga` int(11) NOT NULL,
   `name_usl` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `price` decimal(10,0) NOT NULL,
-  `img` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL
+  `img` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -106,13 +105,13 @@ CREATE TABLE `usluga` (
 --
 
 INSERT INTO `usluga` (`id_usluga`, `name_usl`, `price`, `img`) VALUES
-(1, 'Лабораторные исследования', '1500', ''),
-(2, 'УЗИ', '700', ''),
-(3, 'ЭКГ', '700', ''),
-(4, 'ЭхоКГ', '700', ''),
-(5, 'Вакцинация ', '1200', ''),
-(6, 'Патронажные услуги', '1300', ''),
-(7, 'Медицинская экспертиза', '5000', '');
+(1, 'Лабораторные исследования', '1500', '11.png'),
+(2, 'УЗИ', '700', '9.png'),
+(3, 'ЭКГ', '700', '32.png'),
+(4, 'ЭхоКГ', '700', '1.jpg'),
+(5, 'Вакцинация ', '1200', '8.jpg'),
+(7, 'Медицинская экспертиза', '5000', '31.png'),
+(8, 'МРТ', '1200', '10.png');
 
 -- --------------------------------------------------------
 
@@ -136,7 +135,7 @@ CREATE TABLE `vrach` (
 INSERT INTO `vrach` (`id_vrach`, `id_role`, `fio_vrach`, `img`, `id_spec`, `id_usluga`) VALUES
 (1, 3, 'Лебедева Ирина Григорьевна', '27.jpg', 3, 2),
 (2, 3, 'Кострова Анна Васильевна', '28.jpg', 2, 4),
-(3, 3, 'Сидоров Петр Николаевич', '26.png', 4, 6),
+(3, 3, 'Сидоров Петр Николаевич', '26.png', 4, 5),
 (4, 3, 'Друщиц Виталий Андреевич', '29.png', 7, 1),
 (5, 3, 'Метальников Анатолий Сергеевич', '30.png', 3, 3);
 
@@ -150,11 +149,24 @@ CREATE TABLE `zapis` (
   `id_zapis` int(11) NOT NULL,
   `id_vrach` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
-  `date_zapis` datetime DEFAULT NULL,
+  `date_zapis` date NOT NULL,
+  `time_zapis` time NOT NULL,
   `status` int(11) NOT NULL DEFAULT '0',
   `otziv` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `otvet` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `zapis`
+--
+
+INSERT INTO `zapis` (`id_zapis`, `id_vrach`, `id_user`, `date_zapis`, `time_zapis`, `status`, `otziv`, `otvet`) VALUES
+(1, 2, 1, '2025-02-21', '22:16:13', 1, '123', ''),
+(2, 1, 1, '2025-02-22', '22:16:13', 2, NULL, ''),
+(3, 2, 1, '2025-02-24', '22:16:13', 1, NULL, ''),
+(4, 1, 1, '2025-02-15', '22:16:13', 2, '123123', ''),
+(5, 2, 1, '2025-02-19', '22:16:13', 1, 'hbsdjhdb', ''),
+(6, 1, 1, '2025-02-10', '22:16:13', 2, NULL, '');
 
 --
 -- Индексы сохранённых таблиц
@@ -222,13 +234,13 @@ ALTER TABLE `spec`
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT для таблицы `usluga`
 --
 ALTER TABLE `usluga`
-  MODIFY `id_usluga` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_usluga` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT для таблицы `vrach`
@@ -240,7 +252,7 @@ ALTER TABLE `vrach`
 -- AUTO_INCREMENT для таблицы `zapis`
 --
 ALTER TABLE `zapis`
-  MODIFY `id_zapis` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_zapis` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
@@ -250,22 +262,22 @@ ALTER TABLE `zapis`
 -- Ограничения внешнего ключа таблицы `users`
 --
 ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`id_role`) REFERENCES `role` (`id_role`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`id_role`) REFERENCES `role` (`id_role`);
 
 --
 -- Ограничения внешнего ключа таблицы `vrach`
 --
 ALTER TABLE `vrach`
-  ADD CONSTRAINT `vrach_ibfk_1` FOREIGN KEY (`id_spec`) REFERENCES `spec` (`id_spec`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `vrach_ibfk_2` FOREIGN KEY (`id_usluga`) REFERENCES `usluga` (`id_usluga`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `vrach_ibfk_3` FOREIGN KEY (`id_role`) REFERENCES `role` (`id_role`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `vrach_ibfk_1` FOREIGN KEY (`id_spec`) REFERENCES `spec` (`id_spec`),
+  ADD CONSTRAINT `vrach_ibfk_2` FOREIGN KEY (`id_usluga`) REFERENCES `usluga` (`id_usluga`),
+  ADD CONSTRAINT `vrach_ibfk_3` FOREIGN KEY (`id_role`) REFERENCES `role` (`id_role`);
 
 --
 -- Ограничения внешнего ключа таблицы `zapis`
 --
 ALTER TABLE `zapis`
-  ADD CONSTRAINT `zapis_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `zapis_ibfk_2` FOREIGN KEY (`id_vrach`) REFERENCES `vrach` (`id_vrach`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `zapis_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`),
+  ADD CONSTRAINT `zapis_ibfk_2` FOREIGN KEY (`id_vrach`) REFERENCES `vrach` (`id_vrach`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
