4aedae1f8919170d18a99a5d95add56a678cb8da
savelevroman <160472221+savelevroman@users.noreply.github.com>
2025-03-03 17:35:20 +0700

    Update lkpacient.php

--- lkpacient.php
+++ lkpacient.php
@@ -30,7 +30,7 @@ $result=$mysqli->query($sql);
       $sqll = "SELECT * FROM vrach WHERE id_vrach = ".$row['id_vrach']."";
       $result1=$mysqli->query($sqll);
       $row1 = mysqli_fetch_assoc($result1);
-      if ($row['status'] == 'Новая запись') {
+      if ($row['status'] == 'Новая') {
         echo'
     <tr>
       <td>'.$row1['fio_vrach'].'</td>
@@ -39,7 +39,7 @@ $result=$mysqli->query($sql);
       <td>'.$row['status'].'</td>
     </tr>';
       }
-      elseif ($row['status'] == 'Решено') {
+      elseif ($row['status'] == 'Оказано') {
         if(empty($row['otziv'])){
         echo'
     <tr>
