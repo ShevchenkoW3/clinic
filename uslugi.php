cb675595cb481ed571df8428c2d5550c0f55ebee
ShewchenkoW3 <shevdan03@yandex.ru>
2025-03-10 14:47:28 +0300

    Изменение страниц регистратора

--- uslugi.php
+++ uslugi.php
@@ -9,7 +9,7 @@ $result = $mysqli->query($sql);
     <div class="row">
         <div class="col-lg-12">
             <h2 class="text-center">Услуги</h2>
-            <a href="new_usluga.php" role="button" class="btn btn-primary">Добавить услугу</a>
+            <button type="button" class="btn btn-primary btnnewusluga" data-bs-toggle="modal" data-bs-target="#newUsluga">Добавить услугу</button>
         </div>
     </div><br>
     <div class="row">
@@ -49,7 +49,7 @@ $result = $mysqli->query($sql);
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Записаться на прием</h1>
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Новая цена</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Закрыть"></button>
       </div>
       <div class="modal-body">
@@ -62,6 +62,32 @@ $result = $mysqli->query($sql);
   echo '
   <br><input type="hidden" id="id_usluga" name="id_usluga">'; 
 ?>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>
+        <button type="submit" class="btn btn-primary">Сохранить</button>
+      </div>
+    </form>
+    </div>
+  </div>
+</div>
+<div class="modal fade" id="newUsluga" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Добавить услугу</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Закрыть"></button>
+      </div>
+      <div class="modal-body">
+         <form  method="POST" action="new_usluga.php">
+         <div class="mb-3">
+        <label for="name_usl" class="form-inline">Введите название услуги</label>
+            <input type="text" class="form-control" name="name_usl">
+        </div>
+        <div class="mb-3">
+        <label for="price" class="form-inline">Введите новую цену</label>
+            <input type="text" class="form-control" name="price">
+        </div>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>
@@ -85,6 +111,19 @@ $(document).ready(function(){
   var modal = $(this);
  modal.find('.modal-title').text('Изменить цену услуги');
  modal.find('.modal-body #id_usluga').val(id_usluga);
+})
+});
+</script>
+<script>
+$(document).ready(function(){
+  $('#newUsluga').on('show.bs.modal', function (event) {
+// кнопка, которая вызывает модаль
+ var button = $(event.relatedTarget) 
+   // Здесь изменяем содержимое модали
+  var modal = $(this);
+ modal.find('.modal-title').text('Новая услуга');
+ /* modal.find('.modal-body #name_usl').val(name_usl);
+ modal.find('.modal-body #price').val(price); */
 })
 });
 </script>
