1ca800233fe7c653b085255d061ef89daa6de104
ShewchenkoW3 <shevdan03@yandex.ru>
2025-03-11 13:39:02 +0300

    Update new_usluga.php

--- new_usluga.php
+++ new_usluga.php
@@ -10,22 +10,3 @@ if(!empty($_POST)){
         header("Location: uslugi.php");
 }
 ?>
-<div class="container" style="margin-top: 20px">
-    <div class="row">
-        <div class="col-lg-12">
-            <form action="" class="form-inline" role="form" method="POST">
-        <div class="mb-3">
-            <label for="name_usl" class="form-inline">Введите новую услугу</label>
-            <input type="text" class="form-control" name="name_usl">
-        </div>
-        <div class="mb-3">
-            <label for="price" class="form-inline">Введите цену</label>
-            <input type="number" class="form-control" name="price">
-        </div>
-        <div class="mb-3">
-            <input type="submit" class="btn btn-primary" value="Добавить">
-        </div>
-</form>      
-        </div>
-    </div>
-</div>
